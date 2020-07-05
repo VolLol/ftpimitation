025d4b38633ef17cca574454bca7b612d5056739
VolLol <nurs.ypurs@gmail.com>
2020-07-05 17:43:40 +0300

    Fix ListDirectoryUsecaseTest

--- src/test/java/usecases/ListDirectoryUseCaseTest.java
+++ src/test/java/usecases/ListDirectoryUseCaseTest.java
@@ -18,20 +18,20 @@ public class ListDirectoryUseCaseTest {
         ListDirectoryUseCase listDirectoryUseCase = new ListDirectoryUseCase(sessionContext);
         List<String> answer = listDirectoryUseCase.execute(directory);
 
-        Assert.assertEquals(11, answer.size());
+        Assert.assertEquals(13, answer.size());
         Assert.assertEquals("200 Command okay.", answer.get(0));
         Assert.assertEquals("-----------------",answer.get(1));
         Assert.assertEquals("Type      Name", answer.get(2));
         Assert.assertEquals("-----------------",answer.get(3));
         Assert.assertEquals("dir      classes", answer.get(4));
-        Assert.assertEquals("dir      generated", answer.get(5));
-        Assert.assertEquals("dir      libs", answer.get(6));
-        Assert.assertEquals("dir      reports", answer.get(7));
-        Assert.assertEquals("dir      test-results", answer.get(8));
-        Assert.assertEquals("dir      tmp", answer.get(9));
-        Assert.assertEquals("-----------------", answer.get(10));
-
-
+        Assert.assertEquals("dir      distributions",answer.get(5));
+        Assert.assertEquals("dir      generated", answer.get(6));
+        Assert.assertEquals("dir      libs", answer.get(7));
+        Assert.assertEquals("dir      reports", answer.get(8));
+        Assert.assertEquals("dir      scripts",answer.get(9));
+        Assert.assertEquals("dir      test-results", answer.get(10));
+        Assert.assertEquals("dir      tmp", answer.get(11));
+        Assert.assertEquals("-----------------", answer.get(12));
     }
 
 
